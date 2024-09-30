b5f2520f128b8d8dc23908ff4c9611f62f479165
pallomalacerda <pallomalacerda560@gmail.com>
2024-09-30 19:08:29 -0300

    Add files via upload

---
+++ forca.h
@@ -0,0 +1,12 @@
+#define TAMANHO_PALAVRA 20
+
+int letraexiste(char letra);
+int chuteserrados();
+int enforcou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+int ganhou();
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
